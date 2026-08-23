a61b56af67b52c964b437efac24a3412ab314be0
Ibrahim <ibrahimmiishaq@gmail.com>
2026-08-22 19:59:09 -0700

    Turn the prime checker into an actual calculator
    Added a CountPrimes function that checks every number from 2 up to the user's chosen limit and keeps track of how many of them are prime.
    
    Still completely single-threaded for now because apparently we should make sure the engine starts before we bolt a rocket onto it. Used a really big number and almost crashed my poor laptop...

--- PrimeConcurrentCalculator/main.cpp
+++ PrimeConcurrentCalculator/main.cpp
@@ -23,22 +23,45 @@ bool IsPrime(long long number)
     for (long long i = 5; i * i <= number; i += 6)
     {
         if (number % i == 0 || number % (i + 2) == 0)
+        {
             return false;
+        }
     }
 
     return true;
 }
 
+long long CountPrimes(long long start, long long end)
+{
+    long long primeCount = 0;
+
+    for (long long number = start; number <= end; number++)
+    {
+        if (IsPrime(number))
+            primeCount++;
+    }
+
+    return primeCount;
+}
+
 int main()
 {
+    long long limit;
+
     std::cout << "Prime Calculator\n";
+    std::cout << "================\n\n";
+    std::cout << "Enter the upper limit: ";
+    std::cin >> limit;
 
-    long long testNumbers[] = { 2, 3, 4, 17, 25, 97, 100 };
-
-    for (long long number : testNumbers)
+    if (limit < 2)
     {
-        std::cout << number << (IsPrime(number) ? " is prime.\n" : " is not prime.\n");
+        std::cout << "number must be greater than or equal to 2!\n";
+        return 1;
     }
+
+    long long primeCount = CountPrimes(2, limit);
+
+    std::cout << "\nPrimes found: " << primeCount << "\n";
 
     return 0;
 }
