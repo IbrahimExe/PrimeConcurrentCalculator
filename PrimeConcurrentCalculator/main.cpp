436f8384fb7fbc208b7e2b6145f92786a3696ea8
Ibrahim <ibrahimmiishaq@gmail.com>
2026-08-22 19:42:09 -0700

    Simple Prime Number Checker

--- PrimeConcurrentCalculator/main.cpp
+++ PrimeConcurrentCalculator/main.cpp
@@ -3,19 +3,42 @@
 
 #include <iostream>
 
-int main()
+bool IsPrime(long long number)
 {
-    std::cout << "Prime Concurrency Calculator!\n";
+    if (number <= 1)
+    {
+        return false;
+    }
 
+    if (number <= 3)
+    {
+        return true;
+    }
+
+    if (number % 2 == 0 || number % 3 == 0)
+    { 
+        return false;
+    }
+      
+    for (long long i = 5; i * i <= number; i += 6)
+    {
+        if (number % i == 0 || number % (i + 2) == 0)
+            return false;
+    }
+
+    return true;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+int main()
+{
+    std::cout << "Prime Calculator\n";
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    long long testNumbers[] = { 2, 3, 4, 17, 25, 97, 100 };
+
+    for (long long number : testNumbers)
+    {
+        std::cout << number << (IsPrime(number) ? " is prime.\n" : " is not prime.\n");
+    }
+
+    return 0;
+}
