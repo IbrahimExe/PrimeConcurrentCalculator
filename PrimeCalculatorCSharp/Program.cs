d82a60e8e85ba7841546b2aa175418337b422bae
Ibrahim <ibrahimmiishaq@gmail.com>
2026-08-24 21:10:55 -0700

    Teach the C# project what a prime actually is
    Added the prime checking function and a few basic test values.
    
    This is basically the C# version of the first step from the C++ project, so we know the actual calculation works before we start throwing Tasks at it.

--- PrimeCalculatorCSharp/Program.cs
+++ PrimeCalculatorCSharp/Program.cs
@@ -1,6 +1,43 @@
 ﻿using System;
 
+bool IsPrime(long number)
+{
+    if (number <= 1)
+    {
+        return false;
+    }
+
+    if (number <= 3)
+    {
+        return true;
+    }
+
+    if (number % 2 == 0 || number % 3 == 0)
+    {
+        return false;
+    }
+
+    for (long i = 5; i * i <= number; i += 6)
+    {
+        if (number % i == 0 || number % (i + 2) == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+
 Console.WriteLine("Prime Calculator using C#");
 Console.WriteLine("=================================\n");
-// test
-Console.WriteLine("test");
+
+long[] testNumbers = { 2, 3, 4, 17, 25, 97, 100 };
+
+foreach (long number in testNumbers)
+{
+    Console.WriteLine
+    (
+      $"{number} {(IsPrime(number) ? "is prime." : "is not prime.")}"
+    );
+}
